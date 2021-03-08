a22b909ba31ce58d214793f9ecb0ec4cf6bad462
Sten Linnarsson <sten.linnarsson@ki.se>
2021-03-08 11:05:12 +0100

    renumber clusters

--- cytograph/clustering/dendrogram.py
+++ cytograph/clustering/dendrogram.py
@@ -15,13 +15,17 @@ class Dendrogram(Module):
 		Remarks:
 			Agglomeration using Ward's linkage on the normalized mean expression per cluster.
 			Mean expression values are normalized to the median total UMIs, and `log2(x + 1)` transformed.
+
+			The returned Clusters and ClusterID are renumbered and correspond to the linkage ordering.
 		"""
 		super().__init__(**kwargs)
 
+	@requires("Clusters", "uint32", ("cells",))
 	@requires("SelectedFeatures", "bool", ("genes",))
 	@requires("MeanExpression", "float64", ("clusters", "genes"))
 	@creates("Linkage", "float32", (None, 4))
-	@creates("LinkageOrdering", "uint32", ("clusters",))
+	@creates("ClusterID", "uint32", ("clusters",))
+	@creates("Clusters", "uint32", ("cells",))
 	def fit(self, ws: shoji.WorkspaceManager, save: bool) -> np.ndarray:
 		logging.info(" Dendrogram: Computing normalized expression of selected genes")
 		selected = self.SelectedFeatures[:]
@@ -35,4 +39,21 @@ class Dendrogram(Module):
 		Z = hc.optimal_leaf_ordering(Z, D)
 		ordering = hc.leaves_list(Z)
 
-		return Z, ordering
+		# Renumber the linkage matrix so it corresponds to the reordered clusters
+		for i in range(len(Z)):
+			if Z[i, 0] < x.shape[0]:
+				Z[i, 0] = np.where(Z[i, 0] == ordering)[0]
+			if Z[i, 1] < x.shape[0]:
+				Z[i, 1] = np.where(Z[i, 1] == ordering)[0]
+
+		# Renumber the clusters according to the new ordering
+		clusters = self.Clusters[:]
+		new_clusters = np.zeros(ws.cells.length, dtype="uint32")
+		for i in range(ws.clusters.length):
+			selection = clusters == ordering[i]
+			new_clusters[selection] = i
+
+		# Renumber the cluster IDs (which define the clusters dimension)
+		cluster_ids = np.arange(ordering.shape[0])[ordering]
+
+		return Z, cluster_ids, new_clusters
