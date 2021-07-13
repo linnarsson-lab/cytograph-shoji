bd7166cd1745075b046a69f06c831af285b4450a
Sten Linnarsson <sten.linnarsson@ki.se>
2021-07-13 10:14:03 +0200

    simplify and allow only Excel to weed out bugs

--- cytograph/pipeline/patch_sample_metadata.py
+++ cytograph/pipeline/patch_sample_metadata.py
@@ -3,7 +3,6 @@ from typing import Dict
 import shoji
 import logging
 import sys
-import sqlite3 as sqlite
 import os
 import numpy as np
 from cytograph import Module
@@ -11,20 +10,20 @@ import pandas as pd
 
 
 class PatchSampleMetadata(Module):
-	def __init__(self, db: str, tensors: Dict[str, str], **kwargs) -> None:
+	def __init__(self, table: str, tensors: Dict[str, str], **kwargs) -> None:
 		"""
 		Args:
-			db 				Full path to the metadata database file (a sqlite .db file)
+			table 				Full path to the metadata database file (a sqlite .db file)
 			tensors			Dict of tensors to be loaded from metadata, and their types
 		"""
 		super().__init__(**kwargs)
 
-		self.db = db
-		if not os.path.exists(db):
-			logging.error(f"Samples metadata file '{db}' not found")
+		self.table = table
+		if not os.path.exists(table):
+			logging.error(f"Samples metadata file '{table}' not found")
 			sys.exit(1)
-		if not db.endswith(".db") and not db.endswith(".xlsx"):
-			logging.error(f"Invalid samples metadata file '{db}' (only .db and .xlsx allowed)")
+		if not table.endswith(".xlsx"):
+			logging.error(f"Invalid samples metadata file '{table}' (only .xlsx allowed)")
 			sys.exit(1)
 		self.tensors = tensors
 
@@ -34,39 +33,27 @@ class PatchSampleMetadata(Module):
 
 		samples = self.SampleID[:]
 		for tensor, dtype in self.tensors.items():
-			values = np.zeros(n_cells, dtype=dtype if dtype != "string" else "object")
+			if dtype == "string":
+				values = np.full(n_cells, '', dtype="object")
+			else:
+				values = np.zeros(n_cells, dtype=dtype)
+			table = pd.read_excel(self.table)
 			for sample in np.unique(samples):
+				sampleID = sample.replace("TenX", "10X")
 				mask = (samples == sample)
-				if self.db.endswith(".db"):
-					with sqlite.connect(self.db) as sqldb:
-						cursor = sqldb.cursor()
-						cursor.execute("SELECT * FROM sample WHERE name = ? COLLATE NOCASE", (sample,))
-						keys = [x[0].lower() for x in cursor.description]
-						vals = cursor.fetchone()
-						if vals is None:
-							logging.warning(f"Sample '{sample}' not found in database")
-						else:
-							d = dict(zip(keys, vals))
-							if tensor.lower() not in d:
-								logging.error(f"Tensor '{tensor}' was not found in the metadata")
-								sys.exit(1)
-							values[mask] = d[tensor]
-				elif self.db.endswith(".xlsx"):
-					sampleID = sample.replace("TenX", "10X")
-					table = pd.read_excel(self.db)
-					row = table[table["SampleID"] == sampleID]
-					keys = row.columns.values
-					try:
-						vals = row.values[0]
-					except IndexError as e:
-						logging.error(e)
-						logging.info(f"Tensor {tensor}, SampleID {sample}")
-						sys.exit(1)
-					d = dict(zip(keys, vals))
-					if tensor not in d:
-						logging.error(f"Tensor '{tensor}' was not found in the metadata")
-						sys.exit(1)
-					values[mask] = d[tensor]
+				row = table[table["SampleID"] == sampleID]
+				keys = row.columns.values
+				try:
+					vals = row.values[0]
+				except IndexError as e:
+					logging.error(e)
+					logging.info(f"Tensor {tensor}, SampleID {sample}")
+					sys.exit(1)
+				d = dict(zip(keys, vals))
+				if tensor not in d:
+					logging.error(f"'{tensor}' was not found in the metadata")
+					sys.exit(1)
+				values[mask] = d[tensor]
 			logging.info(f" PatchSampleMetadata: Patching metadata for '{tensor}'")
 			assert values is not None, f"Failed to load metadata for '{tensor}'"
 			ws[tensor] = shoji.Tensor(dtype, ("cells",), inits=values)
