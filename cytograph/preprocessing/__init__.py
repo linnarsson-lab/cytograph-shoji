914d4efdaec24a400b70a56b39b691166301383a
alejandro <alejandro@monod.mbb.ki.se>
2022-09-15 15:04:45 +0200

    QC EEL

--- cytograph/preprocessing/__init__.py
+++ cytograph/preprocessing/__init__.py
@@ -7,7 +7,7 @@ from .cell_summary_statistics import CellSummaryStatistics
 from .gene_summary_statistics import GeneSummaryStatistics
 from .detect_species import DetectSpecies
 from .pearson_residuals_variance import PearsonResidualsVariance
-from .quality_control import QualityControl
+from .quality_control import QualityControl, QualityControlEEL
 from .batch_aware_pearson_residuals import BatchAwarePearsonResiduals
 from .classify_droplets import ClassifyDroplets
 from .compute_cell_hashes import ComputeCellHashes
