655a576d480042b26788d9666f188fd298b199fb
alejandro <alejandro@monod.mbb.ki.se>
2022-09-15 13:15:01 +0200

    changes EEL QC

--- cytograph/preprocessing/quality_control.py
+++ cytograph/preprocessing/quality_control.py
@@ -76,3 +76,58 @@ class QualityControl(Algorithm):
 			logging.info(f" QualityControl: {good_cells.sum()} of {n_cells} cells ({int(100 * good_fraction)}%) passed QC")
 		return (good_cells, np.array(passed_qc, dtype=bool))
 
+class QualityControlEEL(Algorithm):
+	"""
+	Compute QC metrics and mark valid cells (note: consider using ClassifyDroplets instead)
+	"""
+	def __init__(self, min_umis: int = 5, min_genes:int=2, min_fraction_good_cells: float = 0, **kwargs) -> None:
+		"""
+		Args:
+			doublet_threshold		Threshold to call doublets, or "auto" to use automatic threshold (default: "auto")
+			min_umis				Minimum number of UMIs (default: 0)
+			max_mt_fraction			Maximum fraction mitochondrial UMIs (default: 1)
+			min_unspliced_fraction	Minimum fraction unspliced UMIs (default: 0)
+			min_fraction_good_cells	Minimum fraction of cells that must pass QC for the sample to pass as a whole (default: 0)
+		"""
+		super().__init__(**kwargs)
+		self.min_umis = min_umis
+		self.min_fraction_good_cells = min_fraction_good_cells
+
+	@requires("TotalUMIs", "uint32", ("cells",))
+	@creates("ValidCells", "bool", ("cells",))
+	@creates("PassedQC", "bool", ())
+	def fit(self, ws: shoji.WorkspaceManager, save: bool = False) -> Tuple[np.ndarray, np.ndarray]:
+		"""
+		Find cells that pass quality control criteria
+
+		Args:
+			ws				shoji workspace
+			save			if true, save the result to the workspace
+
+		Returns:
+			ValidCells		Bool tensor indicating cells that passed QC criteria
+			PassedQC		Scalar indicating if the sample as a whole passed QC
+		
+		Remarks:
+			The complete Expression and Unspliced tensors are loaded into memory
+			If species is None, cell cycle scores are set to 0
+		"""
+		n_cells = ws.cells.length
+
+		enough_umis = self.TotalUMIs[:] >= self.min_umis
+		enough_genes = (self.Expression[:,:] > 0).sum(axis=1) >= self.min_genes
+		umis_gene_ratio = self.TotalUMIs[:] / enough_genes
+		enough_ratio = umis_gene_ratio > 1
+		good_cells = enough_umis & enough_genes & enough_ratio
+
+		logging.info(f" QualityControl: Marked {n_cells - enough_umis.sum()} cells with too few UMIs")
+
+		passed_qc = True
+		good_fraction = good_cells.sum() / n_cells
+		if good_fraction < self.min_fraction_good_cells:
+			logging.warning(f" QualityControl: Sample failed QC because only {good_cells.sum()} of {n_cells} cells passed")
+			passed_qc = False
+		else:
+			logging.info(f" QualityControl: {good_cells.sum()} of {n_cells} cells ({int(100 * good_fraction)}%) passed QC")
+		return (good_cells, np.array(passed_qc, dtype=bool))
+
