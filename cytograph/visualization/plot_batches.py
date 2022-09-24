256da2925faaaa8e7f06d7ba13c98c6211c23daf
alejandro <alejandro@monod.mbb.ki.se>
2022-09-24 12:55:48 +0200

    m

--- cytograph/visualization/plot_batches.py
+++ cytograph/visualization/plot_batches.py
@@ -65,3 +65,60 @@ class PlotBatches(Algorithm):
 
 		plt.savefig(self.export_dir / (ws._name + "_" + self.filename), dpi=300, bbox_inches='tight')
 		plt.close()
+
+class PlotBatchesEEL(Algorithm):
+	def __init__(self, filename: str = "batches.png", **kwargs) -> None:
+		super().__init__(**kwargs)
+		self.filename = filename
+	
+	@requires("Embedding", "float32", ("cells", 2))
+	def fit(self, ws: shoji.WorkspaceManager, save: bool = False) -> None:
+		logging.info(" PlotBatches: Loading the embedding")
+		xy = self.Embedding[:]
+
+		logging.info(" PlotBatches: Plotting")
+		n_cells = ws.cells.length
+		marker_size = 1000 / np.sqrt(n_cells)
+		plt.figure(figsize=(16, 14))
+
+		def expand_scalars(x):
+			if x.ndim == 0:
+				return np.full(n_cells, fill_value=x)
+			else:
+				return x
+
+		# Age
+		if "Age" in ws:
+			age = expand_scalars(ws.Age[:])
+			plt.subplot(2, 2, 1)
+			plt.scatter(xy[:, 0], xy[:, 1], c=age, s=marker_size, lw=0, cmap="rainbow", vmin=min(age))
+			plt.colorbar(fraction=0.02, pad=0.04)
+			plt.axis("off")
+			plt.title(f"Age")
+
+		# Tissue
+		if "Tissue" in ws:
+			tissue = expand_scalars(ws.Tissue[:])
+			plt.subplot(2, 2, 2)
+			scatterc(xy, c=tissue, s=marker_size, lw=0)
+			plt.axis("off")
+			plt.title(f"Tissue")
+
+		# Chemistry
+		if "Chemistry" in ws:
+			chemistry = expand_scalars(ws.Chemistry[:])
+			plt.subplot(2, 2, 3)
+			scatterc(xy, c=chemistry, s=marker_size, lw=0)
+			plt.axis("off")
+			plt.title(f"Chemistry")
+
+		# Samples
+		if "Sample" in ws:
+			sid = expand_scalars(ws.Sample[:])
+			plt.subplot(2, 2, 4)
+			scatterc(xy, c=sid, s=marker_size, lw=0, legend=None if len(sid) > 20 else "outside")
+			plt.axis("off")
+			plt.title(f"SampleID")
+
+		plt.savefig(self.export_dir / (ws._name + "_" + self.filename), dpi=300, bbox_inches='tight')
+		plt.close()
