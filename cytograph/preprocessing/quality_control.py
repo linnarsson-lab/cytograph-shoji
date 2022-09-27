46e9222a67fdedb4ccb134eaee85602c5f1f133d
alejandro <alejandro@monod.mbb.ki.se>
2022-09-27 13:51:14 +0200

    cahnge default QCEEL

--- cytograph/preprocessing/quality_control.py
+++ cytograph/preprocessing/quality_control.py
@@ -80,8 +80,8 @@ class QualityControlEEL(Algorithm):
 	"""
 	Compute QC metrics and mark valid cells (note: consider using ClassifyDroplets instead)
 	"""
-	def __init__(self, min_umis: int = 5, min_genes:int=2, min_fraction_good_cells: float = 0, 
-		umi_gene_ratio: float = 1.5, **kwargs) -> None:
+	def __init__(self, min_umis: int = 10, min_genes:int=3, min_fraction_good_cells: float = 0, 
+		umi_gene_ratio: float = 2, **kwargs) -> None:
 		"""
 		Args:
 			doublet_threshold		Threshold to call doublets, or "auto" to use automatic threshold (default: "auto")
