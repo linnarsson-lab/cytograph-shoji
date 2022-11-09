26e9bf5e404822f83dd9a74a15e78dce51140cc3
Sten Linnarsson <sten.linnarsson@ki.se>
2022-11-09 23:52:49 +0100

    x

--- cytograph/clustering/more_polished_leiden.py
+++ cytograph/clustering/more_polished_leiden.py
@@ -5,9 +5,6 @@ import igraph
 import numpy as np
 from cytograph import requires, creates, Algorithm
 import shoji
-from sklearn.neighbors import NearestNeighbors
-from sklearn.cluster import DBSCAN
-from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.pipeline import make_pipeline
 from sklearn.calibration import CalibratedClassifierCV
@@ -34,11 +31,7 @@ class MorePolishedLeiden(Algorithm):
 			algorithm (i.e. a tiling of the manifold with clusters of similar size), especially if `max_size` is used
 			to cap the cluster size.
 
-			The polishing step consists of two phases. First, the shape of each cluster on the embedding (e.g. TSNE) is
-			considered using Iglewicz-Hoaglin outlier detection, and clusters with too many outliers are re-clustered
-			using DBSCAN on the embedding. This step is identical to standard PolishedLeiden. 
-			
-			Second, a classifier is trained on all clusters larger than `min_size`, and
+			Next, a classifier is trained on all clusters larger than `min_size`, and
 			all cells are reassigned to their maximum probability cluster. This removes all small clusters and outliers.
 			Note that this can result in clusters larger than `max_size`. The probability of the cluster label for
 			each cell is returned, and can be used to identify outliers or intermediate cell states.
@@ -62,83 +55,6 @@ class MorePolishedLeiden(Algorithm):
 		self.max_size = max_size
 		self.min_size = min_size
 
-	def _is_outlier(self, points: np.ndarray, thresh: float = 3.5) -> np.ndarray:
-		"""
-		Returns a boolean array with True if points are outliers and False
-		otherwise.
-
-		Parameters:
-		-----------
-			points : An numobservations by numdimensions array of observations
-			thresh : The modified z-score to use as a threshold. Observations with
-				a modified z-score (based on the median absolute deviation) greater
-				than this value will be classified as outliers.
-
-		Returns:
-		--------
-			mask : A numobservations-length boolean array.
-
-		References:
-		----------
-			Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect and
-			Handle Outliers", The ASQC Basic References in Quality Control:
-			Statistical Techniques, Edward F. Mykytka, Ph.D., Editor.
-		"""
-		if len(points.shape) == 1:
-			points = points[:, None]
-		median = np.median(points, axis=0)
-		diff = np.sum((points - median)**2, axis=-1)
-		diff = np.sqrt(diff)
-		med_abs_deviation = np.median(diff)
-
-		modified_z_score = 0.6745 * diff / med_abs_deviation
-
-		return modified_z_score > thresh
-
-	def _break_cluster(self, embedding: np.ndarray) -> np.ndarray:
-		"""
-		If needed, split the cluster by density clustering on the embedding
-
-		Returns:
-			An array of cluster labels (all zeros if cluster wasn't split)
-			Note: the returned array may contain -1 for outliers
-		"""
-		# Find outliers in either dimension using Grubbs test
-		xy = PCA().fit_transform(embedding)
-		x = xy[:, 0]
-		y = xy[:, 1]
-		# Standardize x and y (not sure if this is really necessary)
-		x = (x - x.mean()) / x.std()
-		y = (y - y.mean()) / y.std()
-		xy = np.vstack([x, y]).transpose()
-
-		outliers = np.zeros(embedding.shape[0], dtype='bool')
-		for _ in range(5):
-			outliers[~outliers] = self._is_outlier(x[~outliers])
-			outliers[~outliers] = self._is_outlier(y[~outliers])
-
-		# See if the cluster is very dispersed
-		min_pts = min(50, min(x.shape[0] - 1, max(5, round(0.1 * x.shape[0]))))
-		nn = NearestNeighbors(n_neighbors=min_pts, algorithm="ball_tree", n_jobs=4)
-		nn.fit(xy)
-		knn = nn.kneighbors_graph(mode='distance')
-		k_radius = knn.max(axis=1).toarray()
-		epsilon = np.percentile(k_radius, 70)
-		# Not too many outliers, and not too dispersed
-		if outliers.sum() <= 3 and (np.sqrt(x**2 + y**2) < epsilon).sum() >= min_pts * 0.5:
-			return np.zeros(embedding.shape[0], dtype='int')
-
-		# Too many outliers, or too dispersed
-		clusterer = DBSCAN(eps=epsilon, min_samples=round(min_pts * 0.5))
-		labels = clusterer.fit_predict(xy)
-
-		# Assign each outlier to the same cluster as the nearest non-outlier
-		if (labels == -1).sum() > 0:
-			nn = NearestNeighbors(n_neighbors=50, algorithm="ball_tree")
-			nn.fit(xy[labels >= 0])
-			nearest = nn.kneighbors(xy[labels == -1], n_neighbors=1, return_distance=False)
-			labels[labels == -1] = labels[labels >= 0][nearest.flat[:]]
-		return labels
 
 	@requires("Embedding", "float32", ("cells", 2))
 	@requires("Factors", "float32", ("cells", None))
@@ -170,26 +86,6 @@ class MorePolishedLeiden(Algorithm):
 		else:
 			labels = np.array(la.find_partition(g, self.method, weights=weights, max_comm_size=self.max_size, n_iterations=-1).membership)
 		logging.info(f" MorePolishedLeiden: Found {labels.max() + 1} initial clusters")
-
-		# # Break clusters based on the embedding
-		# logging.info(" MorePolishedLeiden: Breaking clusters based on the embedding")
-		# xy = self.Embedding[:]
-		# # Only break clusters that are at least twice as large as the minimum size (note: labels are sorted by cluster size)
-		# cluster_sizes = np.bincount(labels)
-		# next_label = 0
-		# labels2 = np.copy(labels)
-		# for lbl in range(max_label):
-		# 	cluster = labels == lbl
-		# 	if cluster.sum() < self.min_size:
-		# 		continue
-		# 	adjusted = self._break_cluster(xy[cluster, :])
-		# 	new_labels = np.copy(adjusted)
-		# 	for i in range(np.max(adjusted) + 1):
-		# 		new_labels[adjusted == i] = i + next_label
-		# 	next_label = next_label + np.max(adjusted) + 1
-		# 	labels2[cluster] = new_labels
-		# labels = labels2
-		# logging.info(f" MorePolishedLeiden: Found {labels.max() + 1} clusters after breaking clusters on the embedding")
 
 		# Assign each orphan cell to the same cluster as the nearest non-orphan
 		logging.info(f" MorePolishedLeiden: Removing clusters with less than {self.min_size} cells")
--- cytograph/visualization/plot_droplet_classes.py
+++ cytograph/visualization/plot_droplet_classes.py
@@ -10,9 +10,13 @@ from cytograph.visualization.colors import colorize
 
 
 class PlotDropletClasses(Algorithm):
-	def __init__(self, filename: str = "droplets.png", **kwargs) -> None:
-		super().__init__(**kwargs)
-		self.filename = filename
+	def __init__(self, **kwargs) -> None:
+		super().__init__()
+		if "filename" in kwargs:
+			self.filename = kwargs["filename"]
+		else:
+			self.filename = "droplets.png"
+		self.kwargs = kwargs
 
 	@requires("Embedding", "float32", ("cells", 2))
 	@requires("UnsplicedFraction", "float32", ("cells",))
@@ -32,13 +36,22 @@ class PlotDropletClasses(Algorithm):
 		logging.info(" PlotDropletClasses: Plotting")
 		plt.figure(figsize=(10, 10))
 
-		plt.subplot(3,2,1)
+		plt.subplot(3, 2, 1)
 		plt.scatter(u, t, c=mt_frac, cmap="Spectral_r", s=10, lw=0)
+		if "min_unspliced_fraction" in self.kwargs:
+			plt.vlines(self.kwargs["min_unspliced_fraction"], 100, 100_000, linestyles='dashed', lw=1, color="green")
+		if "min_umis" in self.kwargs:
+			plt.hlines(self.kwargs["min_umis"], 0, 1, linestyles='dashed', lw=1, color="green")
+		if "m" in self.kwargs and "k" in self.kwargs:
+			m = self.kwargs["m"]
+			k = self.kwargs["k"]
+			plt.plot([0, 1], [m, m + k], color="green", ls="dashed", lw=1)
 		plt.xlabel("Unspliced fraction")
 		plt.ylabel("Total UMIs")
 		plt.yscale("log")
 		plt.ylim(100, 100_000)
 		plt.xlim(0, 1)
+
 		plt.colorbar(label="Mitochondrial fraction")
 
 		plt.subplot(3,2,2)
@@ -80,12 +93,12 @@ class PlotDropletClasses(Algorithm):
 			txt = plt.text(pos[0], pos[1], str(i), size=10, color="black")
 		plt.scatter(xy[:, 0], xy[:, 1], c=color_clust, s=10, lw=0, alpha=0.1)
 		plt.axis('off')
-        
+
 
 		dropind = np.unique(classes)
 		drop_attr = classes
 		cluster_id, total = np.unique(self.Clusters[:], return_counts=True)
-        
+
 		plt.subplot(3,2,6)
 		bottom_counter = np.zeros(len(cluster_id))
 		for i in dropind[::-1]:
@@ -93,11 +106,11 @@ class PlotDropletClasses(Algorithm):
 			counts = (counts / total) * 100
 			plt.bar(range(len(counts)), counts, bottom=bottom_counter, color=[colors[i]]*len(counts))
 			bottom_counter += counts
-    
+
 		plt.xticks(np.arange(0, len(counts), 1), rotation=90)
 		plt.xlabel('Cluster ID')
 		plt.ylabel('Fraction of droplet class (%)')
-        
+
 		passed = classes == 0
 		plt.suptitle(f"{ws._name} ({ws.Chemistry[:] if 'Chemistry' in ws else '?'}) | {passed.sum()} of {passed.shape[0]} cells passed ({int(passed.sum() / passed.shape[0] * 100)}%)")
 		plt.tight_layout(pad=1.2, h_pad=2.3, w_pad=2.5)
@@ -109,3 +122,4 @@ class PlotDropletClasses(Algorithm):
 		if save:
 			plt.savefig(self.export_dir / (ws._name + "_" + self.filename), dpi=300, bbox_inches='tight')
 			plt.close()
+
