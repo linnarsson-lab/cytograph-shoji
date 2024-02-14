718002d115fed7f550ccdfe901ea4335bbb37f96
Sten Linnarsson <sten.linnarsson@ki.se>
2024-02-14 13:59:06 +0100

    x

--- cytograph/annotation/__init__.py
+++ cytograph/annotation/__init__.py
@@ -5,3 +5,4 @@ from .auto_annotate import AutoAnnotate
 from .karyotyper import Karyotyper
 from .hmm_karyotyper import HmmKaryotyper
 from .neftel_states import NeftelStates
+from .gene_signatures import GeneSignatures
